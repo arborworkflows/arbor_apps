42e85cd37cdd37c9f9633c9c32e1ebb085c1c5ae
Jeffrey Baumes <jeff.baumes@kitware.com>
2017-09-22 21:02:34 -0400

    favicon and some fun style changes

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,109 @@
 <template>
   <div class="app-viewport">
     <md-toolbar class="main-toolbar">
+      <img class="app-logo" src="static/arbor-basic.png">
       <h2 class="md-title" style="flex: 1">Arbor</h2>
     </md-toolbar>
-    <main class="main-content">
-      <girder-file-dialog label="Phylogenetic Tree (.phy)" @change="setTree"></girder-file-dialog>
-      <girder-file-dialog label="Character Matrix (.csv)" @change="setTable"></girder-file-dialog>
 
-      <md-tabs @change="updateActiveTab">
-
-        <md-tab md-label="Phylogenetic tree" :md-active="activeTab === 'tree'">
-          <div>
-            <md-button class="md-icon-button" @click="zoom(0.8)">
-              <md-icon>zoom_out</md-icon>
-            </md-button>
-            <md-button class="md-icon-button" @click="zoom(1.25)">
-              <md-icon>zoom_in</md-icon>
-            </md-button>
-          </div>
-          <div class="scrollable">
-            <div v-if="treeProcessing" class="centered">
-              <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
-            </div>
-            <phylo-tree v-else :tree="treeData" :width="treeWidth" :height="treeHeight"></phylo-tree>
-          </div>
-        </md-tab>
-
-        <md-tab md-label="Character matrix" :md-active="activeTab === 'table'">
-          <div v-if="tableProcessing" class="centered">
+    <md-tabs @change="updateActiveTab">
+      <md-tab md-label="Phylogenetic tree" :md-active="activeTab === 'tree'">
+        <girder-file-dialog label="Phylogenetic Tree (.phy)" @change="setTree"></girder-file-dialog>
+        <div>
+          <md-button class="md-icon-button" @click="zoom(0.8)">
+            <md-icon>zoom_out</md-icon>
+          </md-button>
+          <md-button class="md-icon-button" @click="zoom(1.25)">
+            <md-icon>zoom_in</md-icon>
+          </md-button>
+        </div>
+        <div class="scrollable">
+          <div v-if="treeProcessing" class="centered">
             <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
           </div>
-          <data-table v-else :data="tableData" :columns="tableColumns"></data-table>
-        </md-tab>
+          <phylo-tree v-else :tree="treeData" :width="treeWidth" :height="treeHeight"></phylo-tree>
+        </div>
+      </md-tab>
 
-        <md-tab md-label="Phylogenetic signal" :md-active="activeTab === 'phylogenetic-signal'">
-          <span class="md-caption">Column</span>
-          <div>
-            <div v-for="column in tableColumns" :class="['chip', { 'chip-active': phylogeneticSignal.column === column }]" @click="updatePhylogeneticSignalColumn(column)">{{ column }}</div>
-          </div>
-          <span class="md-subheading centered">{{ phylogeneticSignal.status }}</span>
-          <div v-if="phylogeneticSignal.processing" class="centered">
-            <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
-          </div>
-          <data-table v-else :data="phylogeneticSignal.resultData" :columns="phylogeneticSignal.resultColumns"></data-table>
-        </md-tab>
+      <md-tab md-label="Character matrix" :md-active="activeTab === 'table'">
+        <girder-file-dialog label="Character Matrix (.csv)" @change="setTable"></girder-file-dialog>
+        <div v-if="tableProcessing" class="centered">
+          <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
+        </div>
+        <data-table v-else :data="tableData" :columns="tableColumns"></data-table>
+      </md-tab>
 
-        <md-tab md-label="Ancestral state" :md-active="activeTab === 'ancestral-state'">
-          <span class="md-caption">Column</span>
-          <div>
-            <div v-for="column in tableColumns" :class="['chip', { 'chip-active': ancestralState.column === column }]" @click="updateAncestralStateColumn(column)">{{ column }}</div>
-          </div>
-          <span class="md-subheading centered">{{ ancestralState.status }}</span>
-          <div v-if="ancestralState.processing" class="centered">
-            <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
-          </div>
-          <div v-else>
-            <md-image :md-src="ancestralState.plotImage"></md-image>
-            <data-table :data="ancestralState.resultData" :columns="ancestralState.resultColumns"></data-table>
-          </div>
-        </md-tab>
+      <md-tab v-if="tree.id && table.id" md-label="Phylogenetic signal" :md-active="activeTab === 'phylogenetic-signal'">
+        <span class="md-caption">Column</span>
+        <div>
+          <div v-for="column in tableColumns" :class="['chip', { 'chip-active': phylogeneticSignal.column === column }]" @click="updatePhylogeneticSignalColumn(column)">{{ column }}</div>
+        </div>
+        <span class="md-subheading centered">{{ phylogeneticSignal.status }}</span>
+        <div v-if="phylogeneticSignal.processing" class="centered">
+          <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
+        </div>
+        <data-table v-else :data="phylogeneticSignal.resultData" :columns="phylogeneticSignal.resultColumns"></data-table>
+      </md-tab>
 
-        <md-tab md-label="PGLS" :md-active="activeTab === 'pgls'">
-          <span class="md-caption">X (independent variable)</span>
-          <div>
-            <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pgls.x === column }]" @click="updatePglsX(column)">{{ column }}</div>
-          </div>
-          <span class="md-caption">Y (dependent variable)</span>
-          <div>
-            <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pgls.y === column }]" @click="updatePglsY(column)">{{ column }}</div>
-          </div>
-          <div>
-            <label for="pgls-model">Model for residuals</label>
-            <md-button-toggle id="pgls-model" md-single>
-              <md-button v-for="pglsModel in pglsModels" :class="{ 'md-toggle': pgls.model === pglsModel }" @click="updatePglsModel(pglsModel)">{{ pglsModel }}</md-button>
-            </md-button-toggle>
-          </div>
-          <span class="md-subheading centered">{{ pgls.status }}</span>
-          <div v-if="pgls.processing" class="centered">
-            <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
-          </div>
-          <div v-else>
-            <data-table :data="pgls.resultData" :columns="pgls.resultColumns"></data-table>
-            <md-image :md-src="pgls.plotImage"></md-image>
-          </div>
-        </md-tab>
+      <md-tab v-if="tree.id && table.id" md-label="Ancestral state" :md-active="activeTab === 'ancestral-state'">
+        <span class="md-caption">Column</span>
+        <div>
+          <div v-for="column in tableColumns" :class="['chip', { 'chip-active': ancestralState.column === column }]" @click="updateAncestralStateColumn(column)">{{ column }}</div>
+        </div>
+        <span class="md-subheading centered">{{ ancestralState.status }}</span>
+        <div v-if="ancestralState.processing" class="centered">
+          <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
+        </div>
+        <div v-else>
+          <md-image :md-src="ancestralState.plotImage"></md-image>
+          <data-table :data="ancestralState.resultData" :columns="ancestralState.resultColumns"></data-table>
+        </div>
+      </md-tab>
 
-        <md-tab md-label="PIC" :md-active="activeTab === 'pic'">
-          <span class="md-caption">X (independent variable)</span>
-          <div>
-            <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pic.x === column }]" @click="updatePicX(column)">{{ column }}</div>
-          </div>
-          <span class="md-caption">Y (dependent variable)</span>
-          <div>
-            <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pic.y === column }]" @click="updatePicY(column)">{{ column }}</div>
-          </div>
-          <span class="md-subheading centered">{{ pic.status }}</span>
-          <div v-if="pic.processing" class="centered">
-            <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
-          </div>
-          <div v-else>
-            <data-table :data="pic.summaryData" :columns="pic.summaryColumns"></data-table>
-            <data-table :data="pic.picData" :columns="pic.picColumns"></data-table>
-          </div>
-        </md-tab>
+      <md-tab v-if="tree.id && table.id" md-label="PGLS" :md-active="activeTab === 'pgls'">
+        <span class="md-caption">X (independent variable)</span>
+        <div>
+          <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pgls.x === column }]" @click="updatePglsX(column)">{{ column }}</div>
+        </div>
+        <span class="md-caption">Y (dependent variable)</span>
+        <div>
+          <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pgls.y === column }]" @click="updatePglsY(column)">{{ column }}</div>
+        </div>
+        <div>
+          <label for="pgls-model">Model for residuals</label>
+          <md-button-toggle id="pgls-model" md-single>
+            <md-button v-for="pglsModel in pglsModels" :class="{ 'md-toggle': pgls.model === pglsModel }" @click="updatePglsModel(pglsModel)">{{ pglsModel }}</md-button>
+          </md-button-toggle>
+        </div>
+        <span class="md-subheading centered">{{ pgls.status }}</span>
+        <div v-if="pgls.processing" class="centered">
+          <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
+        </div>
+        <div v-else>
+          <data-table :data="pgls.resultData" :columns="pgls.resultColumns"></data-table>
+          <md-image :md-src="pgls.plotImage"></md-image>
+        </div>
+      </md-tab>
 
-      </md-tabs>
+      <md-tab v-if="tree.id && table.id" md-label="PIC" :md-active="activeTab === 'pic'">
+        <span class="md-caption">X (independent variable)</span>
+        <div>
+          <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pic.x === column }]" @click="updatePicX(column)">{{ column }}</div>
+        </div>
+        <span class="md-caption">Y (dependent variable)</span>
+        <div>
+          <div v-for="column in tableColumns" :class="['chip', { 'chip-active': pic.y === column }]" @click="updatePicY(column)">{{ column }}</div>
+        </div>
+        <span class="md-subheading centered">{{ pic.status }}</span>
+        <div v-if="pic.processing" class="centered">
+          <md-spinner :md-size="150" md-indeterminate class="md-accent"></md-spinner>
+        </div>
+        <div v-else>
+          <data-table :data="pic.summaryData" :columns="pic.summaryColumns"></data-table>
+          <data-table :data="pic.picData" :columns="pic.picColumns"></data-table>
+        </div>
+      </md-tab>
 
-    </main>
+    </md-tabs>
   </div>
 </template>
 
@@ -133,6 +130,7 @@ export default {
     'tableProcessing',
     'tableColumns',
     'tableData',
+    'tree',
     'treeProcessing',
     'treeData',
     'treeWidth',
@@ -167,6 +165,12 @@ export default {
 <style scoped>
 html,
 body,
+.app-logo {
+  height: 32px;
+  width: 32px;
+  margin-right: 8px;
+}
+
 .app-viewport {
   height: 100%;
   overflow: hidden;
